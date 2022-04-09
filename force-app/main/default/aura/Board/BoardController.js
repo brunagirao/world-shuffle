59151b790074aa07d53489c544610c1d2f4e038f
Bruna Girão <brunagirao.s@gmail.com>
2022-04-09 09:09:09 -0300

    BoardController - print array words n winWord

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,7 +1,14 @@
 ({
     doInit : function(component, event, helper) {
         console.log("Initialization completed");
-        console.log("Words: " + helper.getWords(6));
+
+        //get the words
+        const words =  helper.getWords(6);
+        console.log("Words: " + words);
+
+        //get the winWord
+        const winWord = helper.getWinWord(words);
+        console.log("Wind word: " + winWord);
 
     }, 
 })
