acdd268cd64cdf505fe1b3d98f5202ccb83b8151
Bruna Girão <brunagirao.s@gmail.com>
2022-04-09 09:12:49 -0300

    Board & BoardController - create words n winWord aura attributes n set the values

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -4,10 +4,12 @@
 
         //get the words
         const words =  helper.getWords(6);
+        component.set("v.words", words);
         console.log("Words: " + words);
 
         //get the winWord
         const winWord = helper.getWinWord(words);
+        component.set("v.winWord", winWord);
         console.log("Wind word: " + winWord);
 
     }, 
