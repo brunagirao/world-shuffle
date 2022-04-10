7015d9256e1cec12d4f9424072c61cfbdb7381b3
Bruna Girão <brunagirao.s@gmail.com>
2022-04-10 09:00:05 -0300

    BoardPanel - create logic to Start Game Button n communication between  BoardPanel n Board

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
+    startGame : function(component, event, helper) {
+        // access combobox
+        let gameModeCombobox = component.find("gameMode");
+
+        // access combobox value
+        let selectValue = gameModeCombobox.get("v.value");
+
+        // update selectMode attribute
+        component.set("v.selectedMode", selectValue);
+    },
 
     }
 })
