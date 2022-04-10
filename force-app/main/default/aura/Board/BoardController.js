7b5fe176a5d41f588a6c6160cd4890dee6c8236c
Bruna Girão <brunagirao.s@gmail.com>
2022-04-10 08:40:33 -0300

    Board & BoardController - add logic to create the card words

--- force-app/main/default/aura/Board/BoardController.js
+++ force-app/main/default/aura/Board/BoardController.js
@@ -1,16 +1,31 @@
 ({
     doInit : function(component, event, helper) {
         console.log("Initialization completed");
+        
+        //get game mode
+        const gameMode = component.get("v.mode");
+        let column = 0;
+
+        //get the number of columns based on gameMode
+        if (gameMode && gameMode === "hard") {
+            column = 6;
+        } else if (gameMode === "medium") {
+            column = 4;
+        } else {
+            column = 3;
+        }
+
+        //get block size
+        let blockSize = 12/column;
+        component.set("v.blockSize", blockSize);
 
         //get the words
-        const words =  helper.getWords(6);
+        const words =  helper.getWords(column * column);
         component.set("v.words", words);
-        console.log("Words: " + words);
 
         //get the winWord
         const winWord = helper.getWinWord(words);
         component.set("v.winWord", winWord);
-        console.log("Wind word: " + winWord);
 
     }, 
 })
