1e039d3041d9cfbf09ec3f68accbb7893bce85e0
Bruna Girão <brunagirao.s@gmail.com>
2022-04-18 08:44:34 -0300

    Block - add logic to hide tiles

--- force-app/main/default/aura/Block/BlockController.js
+++ force-app/main/default/aura/Block/BlockController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
+    blockClickHandler : function(component, event, helper) {
+        const open = component.get("v.open");
 
+        if (!open) {
+            component.set("v.open", true);
+        }
     }
 })
