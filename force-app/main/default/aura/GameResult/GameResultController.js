ddfdecf654cf142e9b2551445e25ce2d2deb767c
Bruna Girão <brunagirao.s@gmail.com>
2022-04-21 12:04:11 -0300

    GameResult - create datatable and fill columns

--- force-app/main/default/aura/GameResult/GameResultController.js
+++ force-app/main/default/aura/GameResult/GameResultController.js
@@ -1,2 +1,14 @@
 ({
+    doInit : function(component, event, helper) {
+        //define the columns
+        const columns = [
+            {label: 'Game Number',  fieldName: 'Name',          type: 'text'},
+            {label: 'Mode',         fieldName: 'Mode__c',       type: 'text'},
+            {label: 'Played On',    fieldName: 'CreatedDate',   type: 'date'},
+            {label: 'Result',       fieldName: 'Result__c',     type: 'text'},    
+        ];
+        //set the columns variable
+        component.set("v.columns", columns);
+    },
+
 })
