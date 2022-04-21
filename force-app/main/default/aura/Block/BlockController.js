fa24259d9398c9d84b1d413ea53eed8c64572320
Bruna Girão <brunagirao.s@gmail.com>
2022-04-21 07:24:27 -0300

    Block - fix big words on titles using FitText.js

--- force-app/main/default/aura/Block/BlockController.js
+++ force-app/main/default/aura/Block/BlockController.js
@@ -11,5 +11,10 @@
             compEvent.setParams({value : label});
             compEvent.fire();
         }
+    },
+
+    scriptsLoaded : function(component, event, helper) {
+        const divElement = component.getElement(".board-block");
+        fitText(divElement);
     }
 })
